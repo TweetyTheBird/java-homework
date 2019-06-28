54785f811e69000c11d541e4ee6d4b0aeb09911b
TweetyTheBird <42115728+tweetythebird@users.noreply.github.com>
2019-06-28 18:45:09 +0300

    Simplify minMaxCalc method

---
+++ src/com/company/two/HTwoReworkMinMax.java
@@ -0,0 +1,31 @@
+package com.company.two;
+
+import java.util.Scanner;
+
+public class HTwoReworkMinMax {
+    public static void main(String args[]) {
+
+        minMaxCalc();
+
+    }
+
+    public static void minMaxCalc() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: (break on 0)");
+        int testNumber = s.nextInt();
+        if (testNumber == 0) {
+            System.out.println("min and max: 0");
+            return;
+        }
+        int min = testNumber;
+        int max = testNumber;
+
+        while (testNumber != 0) {
+            System.out.println("Enter a number: (break on 0)");
+            testNumber = s.nextInt();
+            min = Math.min(min, testNumber);
+            max = Math.max(max, testNumber);
+        }
+        System.out.println("min: " + min + "; max: " + max);
+    }
+}
